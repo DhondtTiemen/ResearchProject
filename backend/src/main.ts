0337ba1529df916121decef08a2630bdaee18ef4
DhondtTiemen <tiemen.dhondt2@student.howest.be>
2023-01-09 18:33:02 +0100

    setup: backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+  const port: number = +process.env.PORT || 3003
+
+  await app.listen(port)
+  console.info(`Welcome to the server. Visit http://localhost:${port}`)
 }
-bootstrap();
+bootstrap()
