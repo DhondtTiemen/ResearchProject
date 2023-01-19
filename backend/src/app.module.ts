ca78284b9650073b2b5a8fef2aa782dd9ee59f59
DhondtTiemen <tiemen.dhondt2@student.howest.be>
2023-01-19 10:54:37 +0100

    add: info

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
 
 import { typeOrmConfig } from './bootstrap/typeORMConfig'
@@ -6,13 +7,15 @@ import { typeOrmConfig } from './bootstrap/typeORMConfig'
 import { ArtistModule } from './artist/artist.module'
 import { AlbumModule } from './album/album.module'
 import { TrackModule } from './track/track.module'
-import { GenreModule } from './genre/genre.module';
-import { OrderModule } from './order/order.module';
-import { UserModule } from './user/user.module';
+import { GenreModule } from './genre/genre.module'
+import { OrderModule } from './order/order.module'
+import { UserModule } from './user/user.module'
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot(typeOrmConfig),
+
     ArtistModule,
     AlbumModule,
     TrackModule,
